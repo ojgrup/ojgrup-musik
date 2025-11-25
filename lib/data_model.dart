88eb7131cdc37c4c7556a08e3fc6f107e66ec186
ojgrup <108789772+ojgrup@users.noreply.github.com>
2025-11-25 17:39:45 +0800

    Update data_model.dart

--- lib/data_model.dart
+++ lib/data_model.dart
@@ -12,7 +12,7 @@ class AssetSong {
 
 // DAFTAR LAGU GLOBAL (PUBLIC)
 final List<AssetSong> assetSongs = [
-  AssetSong(title: "Lagu Sasak Pertama", artist: "Artis Lombok A", assetPath: "assets/audio/lagu_sasak_1.mp3"),
-  AssetSong(title: "Lagu Sasak Kedua", artist: "Artis Lombok B", assetPath: "assets/audio/lagu_sasak_2.mp3"),
-  AssetSong(title: "Lagu Sasak Ketiga", artist: "Artis Lombok C", assetPath: "assets/audio/lagu_sasak_3.mp3"),
+  AssetSong(title: "Lagu Sasak Pertama", artist: "Artis Lombok A", assetPath: "assets/audio/Peterpan_Kukatakan_Dengan_Indah.mp3"),
+  AssetSong(title: "Lagu Sasak Kedua", artist: "Artis Lombok B", assetPath: "assets/audio/Peterpan_Sally_Sendiri.mp3"),
+  AssetSong(title: "Lagu Sasak Ketiga", artist: "Artis Lombok C", assetPath: "assets/audio/Zias Band - Ku Harus Pergi.mp3"),
 ];
